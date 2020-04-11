2345c7b78d16af464a39bd3f74cb363fc96aa662
Olivera Nedelkovska <olivera.nedelkovska@students.finki.ukim.mk>
2020-04-11 23:40:27 +0200

    Update Student.java
    student class finished

--- Student.java
+++ Student.java
@@ -1,18 +1,69 @@
+import java.util.List;
+
 class Student {
 	String index;
 	String firstName;
 	String lastName;
 	List<Integer> grades;
 
-	//TODO constructor
+	// TODO constructor
+	public Student(String index, String firstName, String lastName, List<Integer> grades) {
+		super();
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grades = grades;
+	}
+	// TODO seters & getters
+	
+	public String getIndex() {
+		return index;
+	}
 
-	//TODO seters & getters
 
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public List<Integer> getGrades() {
+		return grades;
+	}
+
+
+	public void setGrades(List<Integer> grades) {
+		this.grades = grades;
+	}
+	
 	public double getAverage() {
-		//TODO
+		double sum =0;
+		for(Integer grade : grades) {
+			sum += grade;
+		}
+		return sum/grades.size();
 	}
 
 	public int ECTSCredits() {
-		//TODO
+		return grades.size()*6;
 	}
 }
